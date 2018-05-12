f1e91c8131c52eb1fa43887dcc8e1b42688cceaf
dingzhenhao <dingzhenhao@jiuxian.com>
2018-05-12 23:01:01 +0800

    添加Spring boot groovy 语言支持案例

---
+++ springboot-language-groovy/src/main/groovy/org/spring/springboot/SpringbootGroovyApplication.groovy
@@ -0,0 +1,12 @@
+package org.spring.springboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringbootGroovyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run SpringbootGroovyApplication, args
+    }
+}
