ead781baeb0df34d36d33617038ba41ca2a08eb4
dingzhenhao <dingzhenhao@jiuxian.com>
2018-10-15 10:23:07 +0800

    aop

---
+++ springboot-aop/src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,15 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
---
+++ springboot-aop/src/main/java/org/spring/springboot/aspect/UserAccessAspect.java
@@ -0,0 +1,33 @@
+package org.spring.springboot.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class UserAccessAspect {
+
+    @Pointcut(value = "@annotation(org.spring.springboot.aspect.UserAccess)")
+    public void access() {
+
+    }
+
+    @Before("access()")
+    public void deBefore(JoinPoint joinPoint) throws Throwable {
+        System.out.println("second before");
+    }
+
+    @Around("@annotation(userAccess)")
+    public Object around(ProceedingJoinPoint pjp, UserAccess userAccess) {
+        //获取注解里的值
+        System.out.println("second around:" + userAccess.desc());
+        try {
+            return pjp.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return null;
+        }
+    }
+}
