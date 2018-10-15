90d36f92fbdae0773b1ee68d1d014257d3909e0c
dingzhenhao <dingzhenhao@jiuxian.com>
2018-10-15 10:23:24 +0800

    异步

--- springboot-async-methods/src/main/java/org/spring/springboot/SpringbootAsyncMethodsApplication.java
+++ springboot-async-methods/src/main/java/org/spring/springboot/SpringbootAsyncMethodsApplication.java
@@ -2,6 +2,7 @@ package org.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
--- springboot-async-methods/src/main/java/org/spring/springboot/service/impl/NetPageDownService.java
+++ springboot-async-methods/src/main/java/org/spring/springboot/service/impl/NetPageDownService.java
@@ -1,28 +1,43 @@
 package org.spring.springboot.service.impl;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spring.springboot.service.INetPageDownService;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class NetPageDownService implements INetPageDownService {
 
     private static final Logger logger = LoggerFactory.getLogger(NetPageDownService.class);
 
-    private final RestTemplate restTemplate;
+    OkHttpClient okHttpClient=new OkHttpClient.Builder().build();
 
-    public NetPageDownService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
+
 
     @Override
     public String findByUrl(String url) {
+
         logger.info("Looking up " + url);
-        String results = restTemplate.getForObject(url, String.class);
-        // Artificial delay of 1s for demonstration purposes
-        return results;
+        String string = null;
+        try {
+            Request req = new Request.Builder().url(url).build();
+            Call call = okHttpClient.newCall(req);
+            Response execute = call.execute();
+            string = execute.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return string;
     }
 }
